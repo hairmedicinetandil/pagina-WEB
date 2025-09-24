19a0b32b4fc9ccfeae1f2174d2a0395433fc8db1
Franco Maidana <francomaidanaescabues@gmail.com>
2025-09-24 20:27:56 -0300

    malas practicas corregidas

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 btnMenu=document.querySelector("#btn-menu");
 menu=document.querySelector(".nav");
-linkNav=document.querySelectorAll("#link-nav");
+linkNav=document.querySelectorAll(".link-nav");
 btnMenu.addEventListener("click",()=>{
     btnMenu.classList.toggle("girar");
     menu.classList.toggle("mostrar");
